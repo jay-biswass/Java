c41c09fa6085244b467bfbc2dcd8db30ea7cae28
Jay Biswas <jaybiswas725@gmail.com>
2026-01-09 22:54:41 +0530

    init commit

---
+++ src/BasicsOfJava/Variablesint.java
@@ -0,0 +1,9 @@
+package BasicsOfJava;
+
+public class Variablesint {
+    public static void main(String[] args) {
+        int x;
+        x=500;
+        System.out.println(x);
+    }
+}
---
+++ src/BasicsOfJava/basics.java
@@ -0,0 +1,12 @@
+package BasicsOfJava;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("Hello Binod");
+        System.out.println("Jay Biswas");
+        System.out.println("Brainware University");
+        System.out.println("Ending is the new Begining");
+        System.out.println(4);
+        System.out.println(4+7);
+    }
+}
