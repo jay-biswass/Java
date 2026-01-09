122b0ef1685cc1e498b2491f0439c0ddf779591a
Jay Biswas <jaybiswas725@gmail.com>
2026-01-09 23:26:04 +0530

    init commit

---
+++ src/BasicsOfJava/ArithmeticOperations.java
@@ -0,0 +1,13 @@
+package BasicsOfJava;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int x=5;
+        int y=2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+    }
+}
