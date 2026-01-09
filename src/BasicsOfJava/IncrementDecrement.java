81b00aa87eec92bc72ffa3dd275be81fe239a6b4
Jay Biswas <jaybiswas725@gmail.com>
2026-01-10 00:10:16 +0530

    git push origin main

---
+++ src/BasicsOfJava/IncrementDecrement.java
@@ -0,0 +1,21 @@
+package BasicsOfJava;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x=5;
+        System.out.println(x);
+        x++; //iska matlab hai x+1
+        System.out.println(x);
+        x--;
+        x--; //iska matlab hai x-1
+        System.out.println(x);
+
+        // This is a single line comment
+        /*
+         This is
+         a
+         Multiple line
+         Comment.
+         */
+    }
+}
---
+++ src/BasicsOfJava/sumof2.java
@@ -0,0 +1,11 @@
+package BasicsOfJava;
+
+public class sumof2 {
+    public static void main(String[] args) {
+
+    int x = 7;
+    int y = 10;
+    int sum = x+y;
+        System.out.println(sum);
+    }
+}
