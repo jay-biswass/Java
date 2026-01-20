5f33d48c428526282e22dfcbb6d630adf1888ec4
Jay Biswas <jaybiswas725@gmail.com>
2026-01-20 23:03:14 +0530

    git push origin main

---
+++ src/Loops/Table.java
@@ -0,0 +1,14 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println(n+" X "+i+"= "+(i*n));
+        }
+    }
+}
