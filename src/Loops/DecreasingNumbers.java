3cb70675f119d76f3c50cf975ed11c46a8980d31
Jay Biswas <jaybiswas725@gmail.com>
2026-01-21 23:11:40 +0530

    git push origin main

---
+++ src/Loops/DecreasingNumbers.java
@@ -0,0 +1,13 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class DecreasingNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i=10; i>=1; i--){
+            System.out.println(i);
+        }
+
+    }
+}
