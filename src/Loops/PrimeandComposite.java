f59ffd6e277ae09f8c5eea27544298560132b755
Jay Biswas <jaybiswas725@gmail.com>
2026-01-22 23:44:01 +0530

    git push origin main

--- src/Loops/PrimeandComposite.java
+++ src/Loops/PrimeandComposite.java
@@ -1,10 +1,42 @@
+//package Loops;
+//import java.util.Scanner;
+//public class PrimeandComposite {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int n = sc.nextInt();
+//        for(int i=2; i<n; i++){
+//            if (n%i==0){
+//                System.out.println("Composite number");
+//            }
+//            else{
+//                System.out.println("Prime number");
+//            }
+//            break;
+//        }
+//    }
+//}
+//
+//
 package Loops;
-
 import java.util.Scanner;
-
 public class PrimeandComposite {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        boolean flag = false;           //false means prime
+        for(int i=2; i<n; i++){
+            if (n%i==0){
+                flag = true;            //true means composite
+                break;
+            }
+        }
+        if (flag == false){
+            System.out.println("Prime number");
+        }
+        else{
+            System.out.println(("Composite number"));
+        }
     }
 }
