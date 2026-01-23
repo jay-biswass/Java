da3248fd2e36a8e9833a8c5246bbad15dc36fd07
Jay Biswas <jaybiswas725@gmail.com>
2026-01-23 10:58:53 +0530

    git push origin main

---
+++ src/Loops/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i=19;
+        do{
+            System.out.println(i+" ");
+            i++;
+        } while(i<=10);
+
+        }
+    }
+
