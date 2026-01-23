ec3c25a2bdcb3364d8a8beb11305a5504b175840
Jay Biswas <jaybiswas725@gmail.com>
2026-01-23 10:53:31 +0530

    git push origin main

---
+++ src/Loops/ContinueStatement.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class ContinueStatement {
+    public static void main(String[] args) {
+        for (int i=1; i<=30; i++){
+            if (i==14 || i==27) continue;
+            if(i%2==0 || i%3==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
