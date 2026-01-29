30a1df6dbe844798327a179825925dba12493371
Jay Biswas <jaybiswas725@gmail.com>
2026-01-29 22:15:35 +0530

    29/01/26

---
+++ src/Methods/MoreAboutJava.java
@@ -0,0 +1,19 @@
+package Methods;
+public class MoreAboutJava {
+    public static void fun() {
+        for (int i = 1; i <= 10; i++)
+            System.out.print(i+" ");
+    }
+    public static void main(String[] args) {
+        fun();
+        System.out.println("\nYo Yo Honey Singh");
+        fun2();
+        fun3();
+    }
+    public static void fun2() {
+        System.out.println("Hi! Good Morning...");
+    }
+    public static void fun3() {
+        System.out.println("Kaisa hai bro???");
+    }
+}
