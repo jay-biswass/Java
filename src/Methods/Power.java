3907b42b51bc73bc315da18300cbf06256a5e7e6
Jay Biswas <jaybiswas725@gmail.com>
2026-01-29 21:58:16 +0530

    29/01/26

---
+++ src/Methods/Power.java
@@ -0,0 +1,16 @@
+package Methods;
+import java.util.Scanner;
+public class Power {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println(Math.pow(a,b));
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.cbrt(1000));
+        System.out.println(Math.floor(3.14));
+        System.out.println(Math.max(4,7));
+        System.out.println(Math.min(4.6,1.6));
+        System.out.println(Math.abs(-78));
+        System.out.println(Math.max(89,Math.max(58,96)));
+    }
+}
