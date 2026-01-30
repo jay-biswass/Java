859039b3f25112638cae6a5010ace572395b6d0a
Jay Biswas <jaybiswas725@gmail.com>
2026-01-30 23:36:27 +0530

    30/01/26

---
+++ src/Methods/PassingArguments.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class PassingArguments {
+    public static void greet(String name) {
+        System.out.println("Good Morning..."+name);
+    }
+    public static void main(String[] args) {
+        greet("Modi");
+    }
+}
