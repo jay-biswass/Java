5880c68f70393bc898fcd977551b4cb2bc59f84a
Jay Biswas <jaybiswas725@gmail.com>
2026-01-30 23:50:57 +0530

    30/01/26

--- src/Methods/PassingArguments.java
+++ src/Methods/PassingArguments.java
@@ -4,7 +4,14 @@ public class PassingArguments {
     public static void greet(String name) {
         System.out.println("Good Morning..."+name);
     }
+
+    public static void sum(int a, int b, int c){
+        System.out.println("Sum is: "+(a+b+c));
+    }
+
     public static void main(String[] args) {
         greet("Modi");
+
+        sum(70,-17, 44);
     }
 }
