09064720c6f78631c40c3862aff7279ae7078250
Jay Biswas <jaybiswas725@gmail.com>
2026-02-01 10:46:06 +0530

    01/02/26

---
+++ src/Methods/PascalTriangle.java
@@ -0,0 +1,31 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class PascalTriangle {
+    public static int fact(int x){
+        int factx = 1;
+        for (int i=1; i<=x; i++){
+            factx *= i;
+        }
+        return factx;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows :");
+        int n = sc.nextInt();
+
+        for (int i=0; i<=n; i++){
+            for (int j=0; j<=n-i; j++){
+                System.out.print("  "+"");
+            }
+
+            for (int j=0; j<=i; j++){
+                int icj = fact(i)/(fact(j)*fact(i-j));
+                System.out.print(icj +"   ");
+            }
+            System.out.println();
+        }
+    }
+}
