ec513594ff5e5b4d79ed0b7b3bd7948bcb20bf89
Jay Biswas <jaybiswas725@gmail.com>
2026-02-01 12:25:17 +0530

    01/02/26

---
+++ src/Arrays/IOarray.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class IOarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter ther size: ");
+        int n = sc.nextInt();
+        int[] jay = new int[n];
+        System.out.print("Enter the elements: ");
+        for(int i=0; i<n; i++) {
+            jay[i] = sc.nextInt();
+        }
+
+        System.out.print("Array ka double: ");
+            for(int i=0; i<n; i++){
+            System.out.print("Double: "+2* jay[i]+" ");
+                System.out.println("Negative: "+-1*jay[i]+" ");
+        }
+       }
+    }
