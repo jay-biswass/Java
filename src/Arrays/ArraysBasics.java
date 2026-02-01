d1910dbdc00205482205eb79dce7adcbec1fad88
Jay Biswas <jaybiswas725@gmail.com>
2026-02-01 12:25:05 +0530

    01/02/26

---
+++ src/Arrays/ArraysBasics.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class ArraysBasics {
+    public static void main(String[] args) {
+        int[] x = {1,2,3,4,5,6,7};
+        //Printing index
+        System.out.println(x[2]);
+        //updating element
+        x[2] = 69;
+        System.out.println(x[2]);
+        //size of array
+        int[] arr = new int[4];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        System.out.println(arr[3]);
+    }
+}
