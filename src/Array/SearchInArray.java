3f4f73989cc9e6d5733c2ff8258a72000af79687
Jay Biswas <jaybiswas725@gmail.com>
2026-02-04 22:32:41 +0530

    04/02/26

---
+++ src/Array/SearchInArray.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class SearchInArray {
+    public static void main(String[] args) {
+        int[] arr = {12,3,18,27,53,67};
+        int target = 188;
+        boolean flag = false;
+        for (int i=0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                flag = true;
+                break;
+            }
+        }
+        if(flag) System.out.println("Target found in array.");
+        else System.out.println("Target not found.");
+    }
+}
