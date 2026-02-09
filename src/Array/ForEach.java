406fc1e92fb4f12fbbc7abc7c2100ccb826c3a3d
Jay Biswas <jaybiswas725@gmail.com>
2026-02-09 23:41:56 +0530

    09/02/26

---
+++ src/Array/ForEach.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] arr = {5,8,2,6,9,3};
+        for (int jay : arr){
+            System.out.println(jay + " ");
+        }
+    }
+}
