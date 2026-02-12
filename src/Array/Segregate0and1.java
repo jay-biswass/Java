e9279761b503f440b567a48ecf7626eec46b2795
Jay Biswas <jaybiswas725@gmail.com>
2026-02-12 22:55:33 +0530

    12/02/26

--- src/Array/Segregate0and1.java
+++ src/Array/Segregate0and1.java
@@ -1,40 +1,30 @@
 package Array;
 
 public class Segregate0and1 {
-
     public static void main(String[] args) {
-
         int[] arr = {1,0,0,1,1,1,0,1,0,0,1,0,0,1};
-
         segregate0and1(arr);
-
         System.out.print("Sorted array: ");
         for (int ele : arr) {
             System.out.print(ele + " ");
         }
     }
-
     static void segregate0and1(int[] arr) {
-
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left < right) {
-
-            while (left < right && arr[left] == 0) {
-                left++;
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            while (i < j && arr[i] == 0) {
+                i++;
             }
-
-            while (left < right && arr[right] == 1) {
-                right--;
+            while (i < j && arr[j] == 1) {
+                j--;
             }
-
-            if (left < right) {
-                int temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-                left++;
-                right--;
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
             }
         }
     }
