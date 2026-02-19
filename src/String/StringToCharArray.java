d121960d59fb36effcea84e7afba6a7d39a4d3ef
Jay Biswas <jaybiswas725@gmail.com>
2026-02-19 13:26:48 +0530

    19/02/26

---
+++ src/String/StringToCharArray.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StringToCharArray {
+    public static void main(String[] args) {
+        String s = "Sebastian";
+        char[] arr = s.toCharArray();
+        for(char ch:arr){
+            System.out.println(ch);
+        }
+    }
+}
---
+++ src/String/Substring.java
@@ -0,0 +1,10 @@
+package String;
+
+public class Substring {
+    public static void main(String[] args) {
+        String s = "Ravishankar";
+        System.out.println(s.substring(4));
+        System.out.println(s.substring(4,8));
+
+    }
+}
