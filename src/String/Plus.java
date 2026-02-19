9a38f72539ab3ecd39371953877d8af1b73c5b7a
Jay Biswas <jaybiswas725@gmail.com>
2026-02-19 13:27:07 +0530

    19/02/26

---
+++ src/String/Plus.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Plus {
+    public static void main(String[] args) {
+        String s = "Jay ";
+        s += "Biswas is a good boy.";
+        System.out.println(s);
+        System.out.println("Tannu"+60+9);
+
+        String t = "He lives in chandpara";
+        System.out.println(s.concat(t));
+    }
+}
